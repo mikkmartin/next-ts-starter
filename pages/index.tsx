ec1eb01b746fe2778db4f62662feb08c0cedc7b9
Mikk Martin <mikk.martin@gmail.com>
2022-12-01 12:53:13 +0200

    quick update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import styled from '@emotion/styled'
 
-const Title = styled.h1`
-  color: blue;
-  font-size: 50px;
-`
-
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <Container>
+      <h1>Hello world</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <button>Some button</button>
+    </Container>
+  )
 }
+
+const Container = styled.div`
+  font-size: 32px;
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: start;
+`
